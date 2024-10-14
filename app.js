895bffb28f94676c3249a493985a69a7ac7e0a98
Manj Virdee <manjvirdee@Manjs-MBP.Home>
2024-10-14 13:33:00 +0100

    updated middleware to handle all happy endpoints

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require ("express")
 const app = express()
 const { getTopics } = require("./controllers/topics.controllers")
+const endpoints = require ("./endpoints.json")
  
 
 app.use(express.json())
@@ -8,9 +9,14 @@ app.use(express.json())
 
 app.get("/api/topics", getTopics)
 
+app.get("/api", (request, response) => {
+    response.status(200).send({endpoints: endpoints})
+})
 
-app.all("*", (request, response) => {
-    response.status(404).send({msg: "URL NOT FOUND"})
+
+app.all("*", (request, response, next) => {
+    response.status(404).send({message: "URL NOT FOUND"})
+
 });
 
 module.exports = app;
