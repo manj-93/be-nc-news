3dd39e88e7e977c3e216b0e831fef1096582c6ff
Manj Virdee <manjvirdee@Manjs-MBP.Home>
2024-10-15 15:38:06 +0100

    added length test for /api/articles

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -177,32 +177,3 @@ describe("/api/articles", () => {
     });
 });
 
-describe("GET /api/articles/:article_id/comments", () => {
-    it("GET: 200 - responds with an array of comments for the given article_id", () => {
-        return request(app)
-            .get("/api/articles/1/comments")
-            .expect(200)
-            .then(({ body: { comments } }) => {
-                expect(comments).toBeInstanceOf(Array);
-                comments.forEach(comment => {
-                    expect(comment).toMatchObject({
-                        comment_id: expect.any(Number),
-                        votes: expect.any(Number),
-                        created_at: expect.any(String),
-                        author: expect.any(String),
-                        body: expect.any(String),
-                        article_id: expect.any(Number)
-                    });
-                });
-            });
-    });
-
-    it("GET: 400 - responds with an error for an invalid article_id", () => {
-        return request(app)
-            .get("/api/articles/invalid_id/comments")
-            .expect(400)
-            .then(({ body }) => {
-                expect(body).toEqual({ message: "Invalid ID" });
-            });
-    });
-});
