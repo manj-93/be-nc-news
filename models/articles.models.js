2c3cf3f7219a90a9e1157dde693324e5e13de5c7
Manj Virdee <manjvirdee@Manjs-MBP.Home>
2024-10-15 14:24:12 +0100

    articles model updated to select relevant comments

--- models/articles.models.js
+++ models/articles.models.js
@@ -9,3 +9,28 @@ exports.selectArticleById = (article_id) => {
             return rows[0];
         });
 };
+
+exports.selectArticles = (sort_by = "created_at", order = "desc") => {
+    const validColumns = ["article_id", "title", "author", "topic", "created_at", "votes"];
+    const validOrders = ["asc", "desc"];
+
+    if (!validColumns.includes(sort_by)) {
+        return Promise.reject({ status: 400, message: "Invalid sort_by query" });
+    }
+
+    if (!validOrders.includes(order)) {
+        return Promise.reject({ status: 400, message: "Invalid order query" });
+    }
+
+    const queryStr = `
+        SELECT a.article_id, a.title, a.author, a.topic, a.created_at, a.votes, a.article_img_url,
+               COUNT(c.comment_id) AS comment_count
+        FROM articles AS a
+        LEFT JOIN comments AS c ON a.article_id = c.article_id
+        GROUP BY a.article_id
+        ORDER BY ${sort_by} ${order};`;
+
+    return db.query(queryStr).then((result) => {
+        return result.rows;
+    });
+};
