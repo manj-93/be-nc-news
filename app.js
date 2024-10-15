eeb770612f901e162a17900c2e67957dd3daceac
Manj Virdee <manjvirdee@Manjs-MBP.Home>
2024-10-15 14:22:09 +0100

    app.js updated for api/articles

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-const express = require ("express")
-const app = express()
-const { getTopics } = require("./controllers/topics.controllers")
-const endpoints = require ("./endpoints.json")
-const { getArticleById } = require("./controllers/articles.controllers")
- 
+const express = require("express");
+const app = express();
+const { getTopics } = require("./controllers/topics.controllers");
+const endpoints = require("./endpoints.json");
+const { getArticleById, getArticles } = require("./controllers/articles.controllers");
 
-app.use(express.json())
-
-
-app.get("/api/topics", getTopics)
+app.use(express.json());
 
 app.get("/api", (request, response) => {
-    response.status(200).send({endpoints: endpoints})
-})
+    response.status(200).send({ endpoints: endpoints });
+});
 
-app.get("/api/articles/:article_id", getArticleById)
+app.get("/api/topics", getTopics);
 
+app.get("/api/articles/:article_id", getArticleById);
 
-app.all("*", (request, response, next) => {
-    response.status(404).send({message: "URL NOT FOUND"})
+app.get("/api/articles", getArticles);
 
+app.use((err, request, response, next) => {
+    if (err.status) {
+        response.status(err.status).send({ message: err.message });
+    } else {
+        response.status(500).send({ message: "Internal Server Error" });
+    }
+});
+
+app.all("*", (request, response) => {
+    response.status(404).send({ message: "URL NOT FOUND" });
 });
 
 module.exports = app;
