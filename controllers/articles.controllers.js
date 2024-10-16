5b75c3aafbb5142f9b8b0f24c5b593ddf3c64e57
Manj Virdee <manjvirdee@Manjs-MBP.Home>
2024-10-16 17:31:36 +0100

    controllers updated to pass previous tests and POST comments

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,26 +1,22 @@
 const { selectArticleById, selectArticles } = require("../models/articles.models");
-const { selectCommentsByArticleId } = require("../models/comments.models"); 
+const { selectArticleComments } = require("../models/comments.models");
+
 
 exports.getArticleById = (request, response, next) => {
     const { article_id } = request.params;
-
     if (isNaN(article_id)) {
-        return next({ status: 400, message: "Invalid ID" });
+      return next({ status: 400, message: "Invalid ID" });
     }
-
-
     selectArticleById(article_id)
-        .then((article) => {
-            if (!article) {
-                return next({ status: 404, message: "Article not found" });
-            }
-            return selectCommentsByArticleId(article_id)
-                .then(comments => {
-                    response.status(200).send({ article, comments });
-                });
-        })
-        .catch(next);
-};
+      .then((article) => {
+        if (!article) {
+          return next({ status: 404, message: "Article not found" });
+        }
+        response.status(200).send({ article });
+      })
+      .catch(next);
+  };
+
 
 exports.getArticles = (request, response, next) => {
     const { sort_by, order, article_id } = request.query;
@@ -51,7 +47,7 @@ exports.getArticles = (request, response, next) => {
     selectArticles(sort_by || "created_at", order || "desc")
         .then(articles => {
             const articlePromises = articles.map(article => {
-                return selectCommentsByArticleId(article.article_id)
+                return selectArticleComments(article.article_id)
                     .then(comments => {
                         return { ...article, comment_count: comments.length }; 
                     });
@@ -68,15 +64,18 @@ exports.getCommentsByArticleId = (request, response, next) => {
     const { article_id } = request.params;
 
     if (isNaN(article_id) || !Number.isInteger(Number(article_id))) {
-        return next({status: 400, message: "Invalid ID" });
+        return next({ status: 400, message: "Invalid ID" });
     }
 
-    selectArticleById(article_id)
-        .then(() => {
-        return selectCommentsByArticleId(article_id);
-        })
-        .then(comments => {
+    const articlePromise = selectArticleById(article_id);
+    const commentsPromise = selectArticleComments(article_id);
+
+    Promise.all([articlePromise, commentsPromise])
+        .then(([article, comments]) => {
+            if (!article) {
+                return next({ status: 404, message: 'Article not found' });
+            }
             response.status(200).send({ comments });
         })
-        .catch(next)
+        .catch(next);
 };
