947d347eb87d826de1b3804d1a63fa3db4674050
Manj Virdee <manjvirdee@Manjs-MBP.Home>
2024-10-16 17:31:07 +0100

    app.js updated to pass all previous tests

--- app.js
+++ app.js
@@ -2,36 +2,34 @@ const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/topics.controllers");
 const endpoints = require("./endpoints.json");
-const { getArticleById, getArticles} = require("./controllers/articles.controllers");
+const { getArticleById, getArticles } = require("./controllers/articles.controllers");
+const { getComments, postComment } = require("./controllers/comments.controllers");
 
 app.use(express.json());
 
-app.get("/api", (request, response) => {
-    response.status(200).send({ endpoints: endpoints });
+app.get("/api", (req, res) => {
+  res.status(200).send({ endpoints });
 });
 
-app.get("/api/topics", getTopics);
-
-app.get("/api/articles/:article_id", getArticleById);
-
-app.get("/api/articles", getArticles);
-
-app.get("/api/articles/:article_id/comments", getArticleById);
-
+app.get('/api/topics', getTopics);
+app.get('/api/articles', getArticles);
+app.get('/api/articles/:article_id', getArticleById);
+app.get('/api/articles/:article_id/comments', getComments);
 app.post('/api/articles/:article_id/comments', postComment);
 
+app.all("*", (req, res) => {
+  res.status(404).send({ message: "URL NOT FOUND" });
+});
+
 app.use((err, req, res, next) => {
-    if (err.status) {
-        res.status(err.status).send({ message: err.message });
-    } else {
-        res.status(500).send({ message: 'Internal Server Error' });
-    }
+  if (err.code === '22P02') {
+    res.status(400).send({ message: "Invalid ID" });
+  } else if (err.status) {
+    res.status(err.status).send({ message: err.message });
+  } else {
+    console.error(err);
+    res.status(500).send({ message: 'Internal Server Error' });
+  }
 });
-
-
-app.all("*", (request, response) => {
-    response.status(404).send({ message: "URL NOT FOUND" });
-});
-
 
 module.exports = app;
