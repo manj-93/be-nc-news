096e2eae8ae5f10a4f87a10064a8a25d3d265e9d
Manj Virdee <manjvirdee@Manjs-MBP.Home>
2024-10-16 16:41:18 +0100

    final app.js for ticket 6

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
 const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/topics.controllers");
-const endpoints = require("./endpoints.json");
-const { getArticleById, getArticles} = require("./controllers/articles.controllers");
+const { getArticleById, getArticles } = require("./controllers/articles.controllers");
+const { getComments, postComment } = require("./controllers/comments.controllers");
 
 app.use(express.json());
 
-app.get("/api", (request, response) => {
-    response.status(200).send({ endpoints: endpoints });
-});
+app.get('/api/topics', getTopics);
+app.get('/api/articles', getArticles);
+app.get('/api/articles/:article_id', getArticleById);
+app.get('/api/articles/:article_id/comments', getComments);
 
-app.get("/api/topics", getTopics);
-
-app.get("/api/articles/:article_id", getArticleById);
-
-app.get("/api/articles", getArticles);
-
-app.get("/api/articles/:article_id/comments", getArticleById);
 
 app.use((err, req, res, next) => {
-    if (err.status) {
-        res.status(err.status).send({ message: err.message });
-    } else {
-        res.status(500).send({ message: 'Internal Server Error' });
-    }
+  if (err.code === '22P02') {
+    err.status = 400;
+    err.message = "Invalid ID";
+  }
+  next(err);
 });
 
-
-app.all("*", (request, response) => {
-    response.status(404).send({ message: "URL NOT FOUND" });
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ message: err.message });
+  } else {
+    console.error(err);
+    res.status(500).send({ message: 'Internal Server Error' });
+  }
 });
 
 
