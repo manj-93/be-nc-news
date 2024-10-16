be610be232c67b300a2cd92b26516b0d0626457c
Manj Virdee <manjvirdee@Manjs-MBP.Home>
2024-10-16 12:13:22 +0100

    refactored controllers to make them more DRY

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -3,62 +3,45 @@ const { selectCommentsByArticleId } = require("../models/comments.models");
 
 exports.getArticleById = (request, response, next) => {
     const { article_id } = request.params;
-    const id = +article_id;
 
     if (isNaN(article_id)) {
-        return response.status(400).send({ message: "Invalid ID" });
+        return next({status: 400, message: "Invalid ID"});
     }
 
-    selectArticleById(id)
+    selectArticleById(Number(article_id))
         .then((article) => {
-            return selectCommentsByArticleId(id) 
-                .then(comments => {
-                    const commentCount = comments.length;
-                    response.status(200).send({ 
-                        article: { ...article, comment_count: commentCount }
-                    });
-                });
-        })
-        .catch((err) => {
-            if (err.status) {
-                response.status(err.status).send({ message: err.message });
-            } else {
-                next(err);
+            if (!article) {
+                return next({ status: 404, message: "Article not found" });
             }
-        });
+            response.status(200).send({ article });
+        })
+        .catch(next);
 };
 
 exports.getArticles = (request, response, next) => {
     const { sort_by, order, article_id } = request.query;
 
     if (article_id) {
-        const id = Number(article_id);
 
-        return selectArticleById(id)
+        return selectArticleById(article_id)
             .then((article) => {
-                return selectCommentsByArticleId(id) 
+                return selectCommentsByArticleId(article_id) 
                     .then(comments => {
                         response.status(200).send({ article, comments }); 
                     });
             })
-            .catch((err) => {
-                if (err.status) {
-                    response.status(err.status).send({ message: err.message });
-                } else {
-                    next(err);
-                }
-            });
+            .catch(next)
     }
 
     const validSortColumns = ['created_at', 'votes', 'article_id'];
     const validOrders = ['asc', 'desc'];
 
     if (sort_by && !validSortColumns.includes(sort_by)) {
-        return response.status(400).send({ message: 'Invalid sort_by query' });
+        return next({ status: 400, message: 'Invalid sort_by query' });
     }
 
     if (order && !validOrders.includes(order)) {
-        return response.status(400).send({ message: 'Invalid order query' });
+        return next({ status: 400, message: 'Invalid order query' });
     }
 
     selectArticles(sort_by || "created_at", order || "desc")
@@ -81,23 +64,15 @@ exports.getCommentsByArticleId = (request, response, next) => {
     const { article_id } = request.params;
 
     if (isNaN(article_id) || !Number.isInteger(Number(article_id))) {
-        return response.status(400).send({ message: "Invalid ID" });
+        return next({status: 400, message: "Invalid ID" });
     }
 
-    const id = Number(article_id);
-
-    selectArticleById(id)
+    selectArticleById(article_id)
         .then(() => {
-        return selectCommentsByArticleId(id);
+        return selectCommentsByArticleId(article_id);
         })
         .then(comments => {
             response.status(200).send({ comments });
         })
-        .catch((err) => {
-            if (err.status) {
-                response.status(err.status).send({ message: err.message });
-            } else {
-                next(err);
-            }
-        });
+        .catch(next)
 };
