ff6bc50a2014834b7678e012972a91e300455443
Manj Virdee <manjvirdee@Manjs-MBP.Home>
2024-10-16 16:41:48 +0100

    final controllers for ticket 6

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,82 +1,18 @@
 const { selectArticleById, selectArticles } = require("../models/articles.models");
-const { selectCommentsByArticleId } = require("../models/comments.models"); 
 
-exports.getArticleById = (request, response, next) => {
-    const { article_id } = request.params;
-
-    if (isNaN(article_id)) {
-        return next({ status: 400, message: "Invalid ID" });
-    }
-
-
-    selectArticleById(article_id)
-        .then((article) => {
-            if (!article) {
-                return next({ status: 404, message: "Article not found" });
-            }
-            return selectCommentsByArticleId(article_id)
-                .then(comments => {
-                    response.status(200).send({ article, comments });
-                });
-        })
-        .catch(next);
+exports.getArticles = (req, res, next) => {
+  selectArticles()
+    .then(articles => {
+      res.status(200).send({ articles });
+    })
+    .catch(next);
 };
 
-exports.getArticles = (request, response, next) => {
-    const { sort_by, order, article_id } = request.query;
-
-    if (article_id) {
-
-        return selectArticleById(article_id)
-            .then((article) => {
-                return selectCommentsByArticleId(article_id) 
-                    .then(comments => {
-                        response.status(200).send({ article, comments }); 
-                    });
-            })
-            .catch(next)
-    }
-
-    const validSortColumns = ['created_at', 'votes', 'article_id'];
-    const validOrders = ['asc', 'desc'];
-
-    if (sort_by && !validSortColumns.includes(sort_by)) {
-        return next({ status: 400, message: 'Invalid sort_by query' });
-    }
-
-    if (order && !validOrders.includes(order)) {
-        return next({ status: 400, message: 'Invalid order query' });
-    }
-
-    selectArticles(sort_by || "created_at", order || "desc")
-        .then(articles => {
-            const articlePromises = articles.map(article => {
-                return selectCommentsByArticleId(article.article_id)
-                    .then(comments => {
-                        return { ...article, comment_count: comments.length }; 
-                    });
-            });
-            return Promise.all(articlePromises);
-        })
-        .then(articlesWithCounts => {
-            response.status(200).send({ articles: articlesWithCounts });
-        })
-        .catch(next);
-};
-
-exports.getCommentsByArticleId = (request, response, next) => {
-    const { article_id } = request.params;
-
-    if (isNaN(article_id) || !Number.isInteger(Number(article_id))) {
-        return next({status: 400, message: "Invalid ID" });
-    }
-
-    selectArticleById(article_id)
-        .then(() => {
-        return selectCommentsByArticleId(article_id);
-        })
-        .then(comments => {
-            response.status(200).send({ comments });
-        })
-        .catch(next)
+exports.getArticleById = (req, res, next) => {
+  const { article_id } = req.params;
+  selectArticleById(article_id)
+    .then(article => {
+      res.status(200).send({ article });
+    })
+    .catch(next);
 };
