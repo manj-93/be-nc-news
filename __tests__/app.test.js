a91752cbc119713ccbe2f769e1ad2a3a25934868
Manj Virdee <manjvirdee@Manjs-MBP.Home>
2024-10-17 15:02:43 +0100

    get users

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -410,7 +410,7 @@ describe('DELETE /api/comments/:comment_id', () => {
     });
 });
 
-describe.only('GET /api/users', () => {
+describe('GET /api/users', () => {
     test('200: responds with an array of user objects', () => {
       return request(app)
         .get('/api/users')
